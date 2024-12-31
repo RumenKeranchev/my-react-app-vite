96bc98b8ebf89937bf3732c63478d062e62de8f0
Henry E. Jekyll <devtestmail69@gmail.com>
2024-12-31 12:51:00 +0200

    UseEffect code example completed

--- src/tutorial/02-useEffect/starter/01-code-example.jsx
+++ src/tutorial/02-useEffect/starter/01-code-example.jsx
@@ -3,6 +3,16 @@ import { useState } from 'react';
 const CodeExample = () => {
   const [value, setValue] = useState(0);
 
+  const sayHello = () => {
+    console.log('hello');
+
+    // calling setValue here will cause an infinite loop
+    //setValue(value + 1);
+  }
+
+  // runs every time the component renders and re-renders
+  sayHello();
+
   return (
     <div>
       <h1>value : {value}</h1>
