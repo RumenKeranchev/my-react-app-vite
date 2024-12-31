b57cd38edf1a8ae8da63286432fb9e9f5769118b
Henry E. Jekyll <devtestmail69@gmail.com>
2024-12-31 18:13:30 +0200

    Conditional renndering short circuit overview example completed

--- src/tutorial/03-conditional-rendering/starter/04-short-circuit-overview.jsx
+++ src/tutorial/03-conditional-rendering/starter/04-short-circuit-overview.jsx
@@ -1,6 +1,29 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const ShortCircuitOverview = () => {
-  return <h2>short circuit overview</h2>;
+	// falsy
+	const [text, setText] = useState("");
+
+	// truthy
+	const [name, setName] = useState("Jeremy Clarskon");
+
+	return (
+		<>
+			<h2>short circuit overview</h2>
+			<div>Falsy OR : {or(text)}</div>
+			<div>Falsy AND: {and(text)}</div>
+			<div>Truthy OR: {or(name)}</div>
+			<div>Truthy AND: {and(name)}</div>
+		</>
+	);
 };
+
+function or(value) {
+	return value || "default value";
+}
+
+function and(value) {
+	return value && "default value";
+}
+
 export default ShortCircuitOverview;
