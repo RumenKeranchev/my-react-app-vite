715f1f48aabb7866ad59d38b7bc5c9009b5f573f
Henry E. Jekyll <devtestmail69@gmail.com>
2024-12-31 11:55:23 +0200

    UseState Gotcha example completed

--- src/tutorial/01-useState/starter/05-useState-gotcha.jsx
+++ src/tutorial/01-useState/starter/05-useState-gotcha.jsx
@@ -1,5 +1,35 @@
+import { useState } from "react";
+
 const UseStateGotcha = () => {
-  return <h2>useState "gotcha"</h2>;
+	const [value, setValue] = useState(0);
+
+	const hangleIncrease = () => {
+    // asynchronously updating the state
+    // clicking multiple times will increase the value by 1
+		setTimeout(() => {
+			console.log("clicks");
+			setValue((currentState) => {
+				return currentState + 1;
+			});
+		}, 3000);
+    
+    // // synchronously updating the state
+    // // clicking multiple times will NOT increase the value by 1
+    // setTimeout(() => {
+		// 	console.log("clicks");
+		// 	setValue(value + 1);
+		// }, 3000);
+	};
+
+	return (
+		<>
+			<h2>useState "gotcha"</h2>
+			<h3>{value}</h3>
+			<button className="btn" onClick={hangleIncrease}>
+				Increase
+			</button>
+		</>
+	);
 };
 
 export default UseStateGotcha;
