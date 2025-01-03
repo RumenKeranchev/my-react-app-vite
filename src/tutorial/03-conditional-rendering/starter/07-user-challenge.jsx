12be079c1a1b372851fefbfa147ccd904fbd17ec
Henry E. Jekyll <devtestmail69@gmail.com>
2025-01-03 11:59:15 +0200

    Conditional rendering User challange completed

--- src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
+++ src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
@@ -1,5 +1,48 @@
+import { useState } from "react";
+
 const UserChallenge = () => {
-  return <h2>user challenge</h2>;
+	const [user, setUser] = useState(null);
+
+	const callback = (isLogin) => {
+		if (isLogin) {
+			setUser({ name: "john" });
+		} else {
+			setUser(null);
+		}
+	};
+
+	return (
+		<>
+			<h2>user challenge</h2>
+			{user ? (
+				<LogedIn name={user.name} onClick={() => callback(false)} />
+			) : (
+				<LogedOut onClick={() => callback(true)} />
+			)}
+		</>
+	);
+};
+
+const LogedIn = ({ name, onClick }) => {
+	return (
+		<>
+			<h4>hello there, {name}</h4>
+			<button className="btn" onClick={onClick}>
+				Logout
+			</button>
+		</>
+	);
+};
+
+const LogedOut = ({ onClick }) => {
+	return (
+		<>
+			<h4>please login</h4>
+			<button className="btn" onClick={onClick}>
+				Login
+			</button>
+		</>
+	);
 };
 
 export default UserChallenge;
