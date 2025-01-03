189b5bab62de6dafea4188646fa95ed66e423d26
Henry E. Jekyll <devtestmail69@gmail.com>
2025-01-03 12:30:59 +0200

    UseEffect cleanup function example completed

--- src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
+++ src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
@@ -1,5 +1,48 @@
+import { useEffect, useState } from "react";
+
 const CleanupFunction = () => {
-  return <h2>cleanup function</h2>;
+	const [flag, setFlag] = useState(false);
+
+	return (
+		<>
+			<h2>cleanup function</h2>
+
+			<button className='btn' onClick={() => setFlag(!flag)}>
+				Click me!
+			</button>
+
+			{flag && <Heading />}
+		</>
+	);
+};
+
+const Heading = () => {
+	useEffect(() => {
+		console.log("Heading component mounted");
+
+		// Interval will run, even if component is unmounted, so we need to clean it up
+		const id = setInterval(() => {
+			console.log("Interval is running");
+		}, 1000);
+
+		// Cleanup function
+		return () => {
+			clearInterval(id);
+			console.log("cleanup function executed");
+		};
+	}, []);
+
+	return (
+		<h3
+			style={{
+				textTransform: "none",
+				backgroundColor: "#ccc",
+				margin: "1rem",
+			}}
+		>
+			Component is mounted each time is rendered!
+		</h3>
+	);
 };
 
 export default CleanupFunction;
