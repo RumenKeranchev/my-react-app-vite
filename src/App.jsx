891ae554d912b3ebdd2e31047ab0ebf79303d797
Henry E. Jekyll <devtestmail69@gmail.com>
2025-01-07 16:22:36 +0200

    Project structure Navbar example completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import Starter from "./tutorial/02-useEffect/starter/05-cleanup-function";
+// import Starter from "./tutorial/04-project-structure/starter/Navbar/Navbar";
+import Navbar from "./tutorial/04-project-structure/starter/Navbar";
 
 function App() {
 	return (
-		<div className="container">
-			<Starter />
+		<div className='container'>
+			{/* <Starter /> */}
+			<Navbar />
 		</div>
 	);
 }
