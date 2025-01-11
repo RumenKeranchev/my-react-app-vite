994b4c5d3681dc553c368608b5c93a8affee9623
Henry E. Jekyll <devtestmail69@gmail.com>
2025-01-11 20:46:59 +0200

    Forms user challange completed

--- src/tutorial/06-forms/starter/02-user-challenge.jsx
+++ src/tutorial/06-forms/starter/02-user-challenge.jsx
@@ -1,21 +1,105 @@
-const UserChallenge = () => {
-  return (
-    <div>
-      <form className='form'>
-        <h4>Add User</h4>
-        <div className='form-row'>
-          <label htmlFor='name' className='form-label'>
-            name
-          </label>
-          <input type='text' className='form-input' id='name' />
-        </div>
+import { useState } from "react";
+import { data } from "../../../data";
 
-        <button type='submit' className='btn btn-block'>
-          submit
-        </button>
-      </form>
-      {/* render users below */}
-    </div>
-  );
+const User = ({ user, handleDelete }) => {
+	return (
+		<div
+			key={user.id}
+			style={{
+				display: "grid",
+				gridTemplateColumns: "4rem auto",
+				gridTemplateRows: "50px",
+				alignItems: "center",
+				justifyContent: "space-evenly",
+				gap: "10px",
+				background: "#eddfff",
+				borderRadius: "5px",
+				padding: "0 10px",
+			}}
+		>
+			<h4
+				style={{
+					margin: 0,
+					textAlign: "center",
+				}}
+			>
+				{user.name}
+			</h4>
+			<button className='btn' onClick={() => handleDelete(user.id)}>
+				Delete
+			</button>
+		</div>
+	);
 };
+
+const UsersList = ({ users, handleDelete }) => {
+	return (
+		<div
+			style={{
+				width: "510px",
+				display: "grid",
+				margin: "auto",
+				gridTemplateColumns: "1fr 1fr",
+				gap: "10px",
+			}}
+		>
+			{users.map((user) => (
+				<User key={user.id} user={user} handleDelete={handleDelete} />
+			))}
+		</div>
+	);
+};
+
+const UserChallenge = () => {
+	const getId = () =>
+		users && users.length > 0
+			? users.sort((a, b) => b.id - a.id)[0].id + 1
+			: 1;
+
+	const [name, setName] = useState("");
+	const [users, setUsers] = useState(data);
+	const [id, setId] = useState(getId());
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+
+		if (!name) {
+			alert("Please enter a name");
+			return;
+		}
+
+		users.push({ id, name });
+		setUsers(users);
+		setId(getId());
+	};
+
+	const handleDelete = (id) => setUsers(users.filter((u) => u.id !== id));
+
+	return (
+		<div>
+			<form className='form' onSubmit={handleSubmit}>
+				<h4>Add User</h4>
+				<div className='form-row'>
+					<label htmlFor='name' className='form-label'>
+						name
+					</label>
+					<input
+						type='text'
+						className='form-input'
+						id='name'
+						autoComplete='off'
+						value={name}
+						onChange={(e) => setName(e.target.value)}
+					/>
+				</div>
+
+				<button type='submit' className='btn btn-block'>
+					submit
+				</button>
+			</form>
+			{users && <UsersList users={users} handleDelete={handleDelete} />}
+		</div>
+	);
+};
+
 export default UserChallenge;
