2e1b5373c3b01f1ad808a6639442b15c0442c01f
Henry E. Jekyll <devtestmail69@gmail.com>
2025-01-11 21:23:29 +0200

    Forms multiple inputs example completed

--- src/tutorial/06-forms/starter/03-multiple-inputs.jsx
+++ src/tutorial/06-forms/starter/03-multiple-inputs.jsx
@@ -1,35 +1,75 @@
-const MultipleInputs = () => {
-  return (
-    <div>
-      <form className='form'>
-        <h4>Multiple Inputs</h4>
-        {/* name */}
-        <div className='form-row'>
-          <label htmlFor='name' className='form-label'>
-            name
-          </label>
-          <input type='text' className='form-input' id='name' />
-        </div>
-        {/* email */}
-        <div className='form-row'>
-          <label htmlFor='email' className='form-label'>
-            Email
-          </label>
-          <input type='email' className='form-input' id='email' />
-        </div>
-        {/* email */}
-        <div className='form-row'>
-          <label htmlFor='password' className='form-label'>
-            Password
-          </label>
-          <input type='password' className='form-input' id='password' />
-        </div>
+import { useState } from "react";
 
-        <button type='submit' className='btn btn-block'>
-          submit
-        </button>
-      </form>
-    </div>
-  );
+const MultipleInputs = () => {
+	const [user, setUser] = useState({ name: "", email: "", password: "" });
+
+	const handleChange = (e) => {
+		// if (e.target.name === "name") {
+		// 	setUser({ ...user, name: e.target.value });
+		// } else if (e.target.name === "email") {
+		// 	setUser({ ...user, email: e.target.value });
+		// } else if (e.target.name === "password") {
+		// 	setUser({ ...user, password: e.target.value });
+		// }
+
+		setUser({ ...user, [e.target.name]: e.target.value });
+	};
+
+	return (
+		<div>
+			<form className='form'>
+				<h4>Multiple Inputs</h4>
+				{/* name */}
+				<div className='form-row'>
+					<label htmlFor='name' className='form-label'>
+						name
+					</label>
+					<input
+						type='text'
+						className='form-input'
+						id='name'
+						name='name'
+						value={user.name}
+						autoComplete='off'
+						onChange={handleChange}
+					/>
+				</div>
+				{/* email */}
+				<div className='form-row'>
+					<label htmlFor='email' className='form-label'>
+						Email
+					</label>
+					<input
+						type='email'
+						className='form-input'
+						autoComplete='off'
+						id='email'
+						name='email'
+						value={user.email}
+						onChange={handleChange}
+					/>
+				</div>
+				{/* email */}
+				<div className='form-row'>
+					<label htmlFor='password' className='form-label'>
+						Password
+					</label>
+					<input
+						type='password'
+						className='form-input'
+						id='password'
+						name='password'
+						value={user.password}
+						autoComplete='off'
+						onChange={handleChange}
+					/>
+				</div>
+
+				<button type='submit' className='btn btn-block'>
+					submit
+				</button>
+			</form>
+		</div>
+	);
 };
 export default MultipleInputs;
