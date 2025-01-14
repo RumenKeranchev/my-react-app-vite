5f60f836ba98e1cd5248f6d0548250039c69c38a
Henry E. Jekyll <devtestmail69@gmail.com>
2025-01-14 20:19:33 +0200

    Forms other inputs example completed

--- src/tutorial/06-forms/starter/04-other-inputs.jsx
+++ src/tutorial/06-forms/starter/04-other-inputs.jsx
@@ -1,24 +1,47 @@
-import { useState } from 'react';
-const frameworks = ['react', 'angular', 'vue', 'svelte'];
+import { useState } from "react";
+const frameworks = ["react", "angular", "vue", "svelte"];
 const OtherInputs = () => {
-  return (
-    <div>
-      <form className='form'>
-        <h4>Other Inputs</h4>
-        {/* name */}
-        <div className='form-row' style={{ textAlign: 'left' }}>
-          <label htmlFor='shipping'> Free Shipping </label>
-        </div>
-        <div className='form-row' style={{ textAlign: 'left' }}>
-          <label htmlFor='framework' className='form-label'>
-            Framework
-          </label>
-        </div>
-        <button type='submit' className='btn btn-block'>
-          submit
-        </button>
-      </form>
-    </div>
-  );
+	const [shipping, setShipping] = useState(false);
+	const [framework, setFramework] = useState("react");
+
+	return (
+		<div>
+			<form className='form'>
+				<h4>Other Inputs</h4>
+				{/* name */}
+				<div className='form-row' style={{ textAlign: "left" }}>
+					<label htmlFor='shipping'> Free Shipping </label>
+					<input
+						type='checkbox'
+						id='shipping'
+						name='shipping'
+						checked={shipping}
+						onChange={(e) => setShipping(e.target.checked)}
+					/>
+				</div>
+				<div className='form-row' style={{ textAlign: "left" }}>
+					<label htmlFor='framework' className='form-label'>
+						Framework
+					</label>
+					<select
+						name='framework'
+						id='framework'
+						className='form-input'
+						value={framework}
+						onChange={(e) => setFramework(e.target.value)}
+					>
+						{frameworks.map((framework, index) => (
+							<option key={index} value={framework}>
+								{framework}
+							</option>
+						))}
+					</select>
+				</div>
+				<button type='submit' className='btn btn-block'>
+					submit
+				</button>
+			</form>
+		</div>
+	);
 };
 export default OtherInputs;
