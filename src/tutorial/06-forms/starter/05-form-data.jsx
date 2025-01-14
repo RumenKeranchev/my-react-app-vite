4d685f8006f3d8d7c0d218c34399176541c5b66c
Henry E. Jekyll <devtestmail69@gmail.com>
2025-01-14 20:28:55 +0200

    Forms form data example completed

--- src/tutorial/06-forms/starter/05-form-data.jsx
+++ src/tutorial/06-forms/starter/05-form-data.jsx
@@ -1,47 +1,65 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const UncontrolledInputs = () => {
-  const [value, setValue] = useState(0);
+	const [value, setValue] = useState(0);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-  };
-  return (
-    <div>
-      <form className='form' onSubmit={handleSubmit}>
-        <h4>Form Data API</h4>
-        {/* name */}
-        <div className='form-row'>
-          <label htmlFor='name' className='form-label'>
-            name
-          </label>
-          <input type='text' className='form-input' id='name' name='name' />
-        </div>
-        {/* email */}
-        <div className='form-row'>
-          <label htmlFor='email' className='form-label'>
-            Email
-          </label>
-          <input type='email' className='form-input' id='email' name='email' />
-        </div>
-        {/* email */}
-        <div className='form-row'>
-          <label htmlFor='password' className='form-label'>
-            Password
-          </label>
-          <input
-            type='password'
-            className='form-input'
-            id='password'
-            name='password'
-          />
-        </div>
+	const handleSubmit = (e) => {
+		e.preventDefault();
 
-        <button type='submit' className='btn btn-block'>
-          submit
-        </button>
-      </form>
-    </div>
-  );
+		const formData = new FormData(e.currentTarget);
+		console.log(formData.get("name"));
+
+		const person = Object.fromEntries(formData.entries());
+		console.log(person);
+
+		e.currentTarget.reset();
+	};
+	return (
+		<div>
+			<form className='form' onSubmit={handleSubmit}>
+				<h4>Form Data API</h4>
+				{/* name */}
+				<div className='form-row'>
+					<label htmlFor='name' className='form-label'>
+						name
+					</label>
+					<input
+						type='text'
+						className='form-input'
+						id='name'
+						name='name'
+					/>
+				</div>
+				{/* email */}
+				<div className='form-row'>
+					<label htmlFor='email' className='form-label'>
+						Email
+					</label>
+					<input
+						type='email'
+						className='form-input'
+						id='email'
+						name='email'
+					/>
+				</div>
+				{/* email */}
+				<div className='form-row'>
+					<label htmlFor='password' className='form-label'>
+						Password
+					</label>
+					<input
+						type='password'
+						className='form-input'
+						id='password'
+						name='password'
+					/>
+				</div>
+
+				<button type='submit' className='btn btn-block'>
+					submit
+				</button>
+			</form>
+		</div>
+	);
 };
 export default UncontrolledInputs;
